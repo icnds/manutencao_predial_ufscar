a18d764baa207562e95f5306dadfbfe4f8c3dea8
Isabel de Sousa <icn.sousa@proton.me>
2026-04-04 16:37:37 -0300

    Add frequencies options (selector)

--- pages/1_Categorias.py
+++ pages/1_Categorias.py
@@ -31,87 +31,156 @@ CONN = sqlite3.connect('manutencao_predial_ufscar.db')
 # --- #
 # Categorias geral
 # --- #
-categorias_geral = get_data(query="""
-                            SELECT CATEGORIA, COUNT(*) as QUANTIDADE 
-                            FROM sao_carlos 
-                            GROUP BY CATEGORIA
-                            ORDER BY QUANTIDADE DESC;
-                            """, conn=CONN)
-
 
 st.markdown('## Distribuição das categorias de manutenção predial')
 
-# Texto interpretativo em fonte pequena
-st.caption('''
-           A categoria materiais corresponde a mais da metade 
-           das rotinas de manutenção predial.
-           ''', text_alignment='left')
+# Seletor
+grafico = st.selectbox(
+    'Gráfico:',
+    options=('Quantidade', 'Porcentagem'), 
+    label_visibility='hidden'
+)
 
-# Exibe o DataFrame 
-st.table(categorias_geral, border='horizontal')
+if grafico == 'Quantidade':
+    # Consulta SQL para obtenção do DataFrame
+    categorias_geral = get_data(query="""
+                                SELECT CATEGORIA, COUNT(*) as QUANTIDADE 
+                                FROM sao_carlos 
+                                GROUP BY CATEGORIA
+                                ORDER BY QUANTIDADE DESC;
+                                """, conn=CONN)
 
-# Configura tamanho da figura e cor do fundo
-plt.figure(figsize=(7, 4), facecolor='#1D293D')
+    # Texto interpretativo em fonte pequena
+    st.caption('''
 
-# Cria gráfico de barras
-custom_palette = ['#0068C9', '#FFABAB', '#83C9FF']
-sns.barplot(data=categorias_geral, x='CATEGORIA', y='QUANTIDADE', palette=custom_palette, legend=False)
+               ''', text_alignment='left')
 
-# Altera a cor de fundo dos eixos
-plt.gca().set_facecolor('#1D293D')
+    # Exibe o DataFrame 
+    st.table(categorias_geral, border='horizontal')
 
-# Adiciona título e rótulos
-plt.xlabel('')
-plt.ylabel('N', color='white')
-plt.xticks(rotation=0, color='white')
+    # Configura tamanho da figura e cor do fundo
+    plt.figure(figsize=(7, 4), facecolor='#1D293D')
 
-# Altera a cor dos eixos
-ax = plt.gca()
-for spine in ax.spines.values():
-    spine.set_visible(False)
+    # Cria gráfico de barras
+    custom_palette = ['#0068C9', '#FFABAB', '#83C9FF']
+    sns.barplot(data=categorias_geral, x='CATEGORIA', y='QUANTIDADE', palette=custom_palette, legend=False)
 
-for tick in ax.get_yticklabels():
-    tick.set_color('white')
+    # Altera a cor de fundo dos eixos
+    plt.gca().set_facecolor('#1D293D')
 
-# Adiciona grade
-ax.yaxis.grid(color='white', linestyle='--', linewidth=0.35, alpha=0.8)
-ax.xaxis.grid(False)
+    # Adiciona título e rótulos
+    plt.xlabel('')
+    plt.ylabel('N', color='white')
+    plt.xticks(rotation=0, color='white')
 
-# Calcula proporção e multiplica por 100 para obter porcentagens
-categorias_geral['PROPORCAO'] = (categorias_geral['QUANTIDADE'] / categorias_geral['QUANTIDADE'].sum()) * 100
+    # Altera a cor dos eixos
+    ax = plt.gca()
+    for spine in ax.spines.values():
+        spine.set_visible(False)
 
-# Adiciona valores e porcentagens acima das barras
-for p in ax.patches:
-    altura = int(p.get_height())
-    proporcao = f'{altura} ({p.get_height() / categorias_geral['QUANTIDADE'].sum() * 100:.1f}%)'
-    ax.annotate(proporcao, (p.get_x() + p.get_width() / 2., p.get_height()),
-                ha='center', va='bottom', color='white', fontsize=10)
+    for tick in ax.get_yticklabels():
+        tick.set_color('white')
 
-# Define limite do eixo y
-plt.ylim(0, categorias_geral['QUANTIDADE'].max() * 1.1)  # Para dar um espaço no topo
+    # Adiciona grade
+    ax.yaxis.grid(color='white', linestyle='--', linewidth=0.35, alpha=0.8)
+    ax.xaxis.grid(False)
 
-# Ajusta layout
-plt.tight_layout()
+    # Adiciona valores e porcentagens acima das barras
+    for p in ax.patches:
+        altura = int(p.get_height())
+        quantidade = f'{altura}'
+        ax.annotate(quantidade, (p.get_x() + p.get_width() / 2., p.get_height()),
+                   ha='center', va='bottom', color='white', fontsize=10)
 
-# Exibe gráfico no Streamlit
-st.pyplot(plt)
+    # Define limite do eixo y
+    plt.ylim(0, categorias_geral['QUANTIDADE'].max() * 1.2)  # Para dar um espaço no topo
 
+    # Ajusta layout
+    plt.tight_layout()
+
+    # Exibe gráfico no Streamlit
+    st.pyplot(plt)
+    
+else:
+    # Consulta SQL para obtenção do DataFrame
+    categorias_geral = get_data(query="""
+                                SELECT CATEGORIA, 
+                                COUNT(*) * 1.0 / (SELECT COUNT(*) FROM sao_carlos) AS PERCENTUAL
+                                FROM sao_carlos 
+                                GROUP BY CATEGORIA
+                                ORDER BY PERCENTUAL DESC;
+                                """, conn=CONN)
+
+    # Texto interpretativo em fonte pequena
+    st.caption('''
+
+            ''', text_alignment='left')
+
+    # Exibe o DataFrame 
+    categorias_geral['PERCENTUAL'] = categorias_geral['PERCENTUAL'] * 100
+    st.table(categorias_geral, border='horizontal')
+
+    # Configura tamanho da figura e cor do fundo
+    plt.figure(figsize=(7, 4), facecolor='#1D293D')
+
+    # Cria gráfico de barras para a proporção
+    custom_palette = ['#0068C9', '#FFABAB', '#83C9FF']
+    sns.barplot(data=categorias_geral, x='CATEGORIA', y='PERCENTUAL', palette=custom_palette, legend=False)
+
+    # Altera a cor de fundo dos eixos
+    plt.gca().set_facecolor('#1D293D')
+
+    # Adiciona título e rótulos
+    plt.xlabel('')
+    plt.ylabel('%', color='white')
+    plt.xticks(rotation=0, color='white')
+
+    # Altera a cor dos eixos
+    ax = plt.gca()
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    for tick in ax.get_yticklabels():
+        tick.set_color('white')
+
+    # Adiciona grade
+    ax.yaxis.grid(color='white', linestyle='--', linewidth=0.35, alpha=0.8)
+    ax.xaxis.grid(False)
+
+    # Adiciona valores e porcentagens acima das barras
+    for p in ax.patches:
+        altura = p.get_height()
+        proporcao = f'{altura:.0f}%'  # Formato percentual
+        ax.annotate(proporcao, (p.get_x() + p.get_width() / 2., altura),
+                    ha='center', va='bottom', color='white', fontsize=10)
+
+    # Define limite do eixo y
+    plt.ylim(0, 100)  # Para dar espaço no topo
+
+    # Ajusta layout
+    plt.tight_layout()
+
+    # Exibe gráfico no Streamlit
+    st.pyplot(plt)
+    
 # --- #
 # Categorias temporal
 # --- #
+
 st.markdown('## Categorias de manutenção predial ao longo do tempo')
 
 # Seletor
-option = st.selectbox(
+anos = ('2023', '2024', '2025', 'Todos')
+periodo = st.selectbox(
     'Período:',
-    ('2023', '2024', '2025', 'Todos'),
-    index=None,
-    placeholder='Selecione o período...',
+    options=anos,
+    index=len(anos) - 1, 
+    label_visibility='hidden'
 )
 
-option_safe = str(option).replace("'", "''")
+option_safe = str(periodo).replace("'", "''")
 
-if option == 'Todos':
+if periodo == 'Todos':
     categorias_temporal = get_data(query="""
                                    SELECT strftime('%Y-%m', DATA) AS MÊS, CATEGORIA, COUNT(*) as QUANTIDADE 
                                    FROM sao_carlos 
@@ -122,10 +191,10 @@ if option == 'Todos':
     categorias_temporal_pivot = categorias_temporal.pivot(index='MÊS', columns='CATEGORIA', values='QUANTIDADE').fillna(0)
 
     st.caption('''
-            A categoria Equipe tem dois picos mais acentuados em junho de 2024 e de 2025.
-            ''', text_alignment='center')
 
-    st.line_chart(data=categorias_temporal_pivot, use_container_width=True)
+               ''', text_alignment='center')
+
+    st.line_chart(data=categorias_temporal_pivot, width='stretch')
 
 
 else:
@@ -140,10 +209,10 @@ else:
     categoria_ano_pivot = categoria_ano.pivot(index='MÊS', columns='CATEGORIA', values='QUANTIDADE').fillna(0)
 
     st.caption('''
-            
-            ''', text_alignment='center')
+
+               ''', text_alignment='center')
     
-    st.line_chart(data=categoria_ano_pivot, use_container_width=True)
+    st.line_chart(data=categoria_ano_pivot, width='stretch')
 
 # Fecha conexão
 CONN.close()
