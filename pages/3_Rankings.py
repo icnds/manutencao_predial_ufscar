6edeaa163ade293162cf86757164da1a080831ef
Isabel de Sousa <icn.sousa@proton.me>
2026-05-10 17:08:33 -0300

    Added rankings plots

--- pages/3_Rankings.py
+++ pages/3_Rankings.py
@@ -3,7 +3,6 @@ import sqlite3
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib.ticker import FuncFormatter
 
 # ------------- #
 # CONFIGURAÇÕES #
@@ -18,9 +17,7 @@ st.set_page_config(
 # CONSTANTES #
 # ---------- #
 
-CUSTOM_PALETTE = {'EQUIPE': '#83C9FF', 
-         'SERVIÇOS / EQUIPAMENTOS': '#FFABAB', 
-         'MATERIAIS': '#0068C9'}
+BAR_COLOR = '#73953D'
 FACECOLOR = '#1D293D'
 
 # ------- #
@@ -29,6 +26,62 @@ FACECOLOR = '#1D293D'
 
 def get_data(query, conn):
     return pd.read_sql_query(query, conn)
+
+
+def exibir_tabela(df, tipo):
+    tabela = df.copy()
+    tabela.loc[tabela['CLASSIFICAÇÃO SINAPI'] == 'LIVRO SINAPI: CÁLCULOS E PARÂMETROS', 'CLASSIFICAÇÃO SINAPI'] = 'EQUIPE ROTINEIRA (PROFISSIONAIS / HORA)'
+    if tipo == 'valor':
+        tabela['TOTAL'] = tabela['TOTAL'].apply(lambda x: f'R$ {x:,.2f}'.replace(',', '_').replace('.', ',').replace('_', '.'))
+    else:
+        tabela['TOTAL'] = tabela['TOTAL'].apply(lambda x: f'{x:,.2f}'.replace(',', '_').replace('.', ',').replace('_', '.'))
+    return tabela
+
+def exibir_grafico_barras(df, tipo):
+
+    tabela = df.copy()
+    tabela.loc[tabela['CLASSIFICAÇÃO SINAPI'] == 'LIVRO SINAPI: CÁLCULOS E PARÂMETROS', 'CLASSIFICAÇÃO SINAPI'] = 'EQUIPE ROTINEIRA (PROFISSIONAIS / HORA)'
+
+    # Configura tamanho da figura e cor de fundo
+    plt.figure(figsize=(7, 4), facecolor=FACECOLOR)
+
+    # Gráfico de barras horizontais
+    sns.barplot(tabela, x= 'TOTAL', y='CLASSIFICAÇÃO SINAPI', color=BAR_COLOR, orient='y')
+
+    # Altera cor de fundo dos eixos
+    plt.gca().set_facecolor(FACECOLOR)
+
+    # Adiciona título e rótulos
+    plt.xlabel('')
+    plt.ylabel('')
+
+    plt.xticks([])
+    plt.yticks(color='white')
+
+    # Altera cor dos eixos
+    ax = plt.gca()
+
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    for tick in ax.get_xticklabels():
+        tick.set_color('white')
+
+    # Adiciona valores acima das barras
+    for p in ax.patches:
+        width = float(p.get_width())
+        if tipo == 'valor':
+            quantidade = f'R$ {width:,.2f}'.replace(',', '_').replace('.', ',').replace('_', '.')
+        else:
+            quantidade = f'{width:,.2f}'.replace(',', '_').replace('.', ',').replace('_', '.')
+        ax.annotate(quantidade, (width, p.get_y() + p.get_height() / 2), 
+                    ha='left', va='center', color='white', fontsize=10)
+
+    # Ajusta layout
+    plt.tight_layout()
+
+    # Exibe gráfico no Streamlit
+    st.pyplot(plt)
 
 # ------------------------------------- #
 # TÍTULO, APRESENTACAO E CONEXÃO SQLite #
@@ -59,38 +112,134 @@ periodo = st.selectbox(
     label_visibility='hidden'
 )
 
-st.markdown('## Recorrência por categoria e tabela de consulta')
+# ------- #
+# TABELAS #
+# ------- #
 
-# Top 10 por Valor (Onde o dinheiro está indo)
-df = get_data(query="""
-              SELECT `CLASSIFICAÇÃO SINAPI`, SUM(`TOTAL DA ETAPA`) AS TOTAL
-              FROM sao_carlos
-              WHERE `CLASSIFICAÇÃO SINAPI` NOT IN ('LIVRO SINAPI: CÁLCULOS E PARÂMETROS', 'NÃO SINAPI', 'NÃO ENCONTRADO', 'MATERIAL')
-              GROUP BY `CLASSIFICAÇÃO SINAPI`
-              ORDER BY TOTAL DESC
-              LIMIT 10;
-              """, conn=CONN)
-    
-# DataFrame completo
-st.caption('''
-                            
-            ''', text_alignment='left')
+if periodo == '2023 - 2025':
 
-st.table(df, border='horizontal')
+    on = st.toggle('R$', key='valor')
+    if on:
+        st.markdown('## Top 10 por Valor')
+        st.markdown('#### Para onde o dinheiro está indo')
 
-# Top 10 por Quantidade (O que dá mais trabalho operacional)
-df = get_data(query="""
-              SELECT `CLASSIFICAÇÃO SINAPI`, SUM(`QUANT.`) AS TOTAL
-              FROM sao_carlos
-              WHERE `CLASSIFICAÇÃO SINAPI` NOT IN ('LIVRO SINAPI: CÁLCULOS E PARÂMETROS', 'NÃO SINAPI', 'NÃO ENCONTRADO', 'MATERIAL')
-              GROUP BY `CLASSIFICAÇÃO SINAPI`
-              ORDER BY TOTAL DESC
-              LIMIT 10;
-              """, conn=CONN)
-    
-# DataFrame completo
-st.caption('''
-                            
-            ''', text_alignment='left')
+        # Valor
+        df = get_data(query="""
+                    SELECT `CLASSIFICAÇÃO SINAPI`, SUM(`TOTAL DA ETAPA`) AS TOTAL
+                    FROM sao_carlos
+                    WHERE `CLASSIFICAÇÃO SINAPI` NOT IN ('NÃO SINAPI', 'NÃO ENCONTRADO')
+                    GROUP BY `CLASSIFICAÇÃO SINAPI`
+                    HAVING TOTAL > 0
+                    ORDER BY TOTAL DESC
+                    LIMIT 10;
+                    """, conn=CONN)
+        
+        # # Formata DataFrame
+        # tabela = exibir_tabela(df, tipo='valor')
+            
+        # # DataFrame completo
+        # st.caption('''
+                                    
+        #             ''', text_alignment='left')
 
-st.table(df, border='horizontal')
+        # st.table(tabela, border='horizontal')
+
+        exibir_grafico_barras(df, tipo='valor')
+
+    else: 
+        st.markdown('## Top 10 por Quantidade')
+        st.markdown('#### O que dá mais trabalho operacional')
+
+        # Quantidade
+        df = get_data(query="""
+                    SELECT `CLASSIFICAÇÃO SINAPI`, SUM(`QUANT.`) AS TOTAL
+                    FROM sao_carlos
+                    WHERE `CLASSIFICAÇÃO SINAPI` NOT IN ('NÃO SINAPI', 'NÃO ENCONTRADO')
+                    GROUP BY `CLASSIFICAÇÃO SINAPI`
+                    HAVING TOTAL > 0
+                    ORDER BY TOTAL DESC
+                    LIMIT 10;
+                    """, conn=CONN)
+        
+        # # Formata DataFrame
+        # tabela = exibir_tabela(df, tipo='quantidade')
+            
+        # # DataFrame completo
+        # st.caption('''
+                                    
+        #             ''', text_alignment='left')
+
+        # st.table(tabela, border='horizontal')
+
+        exibir_grafico_barras(df, tipo='quantidade')
+else:
+    option_safe = str(periodo).strip()
+
+    on = st.toggle('R$', key='valor')
+    if on:
+        st.markdown('## Top 10 por Valor')
+        st.markdown('#### Para onde o dinheiro está indo')
+
+        # Valor
+        df = get_data(query=f"""
+                    SELECT `CLASSIFICAÇÃO SINAPI`, SUM(`TOTAL DA ETAPA`) AS TOTAL
+                    FROM sao_carlos
+                    WHERE `CLASSIFICAÇÃO SINAPI` NOT IN (
+                      'LIVRO SINAPI: CÁLCULOS E PARÂMETROS', 
+                      'NÃO SINAPI', 
+                      'NÃO ENCONTRADO', 
+                      'MATERIAL'
+                      ) 
+                      AND strftime('%Y', DATA) = '{option_safe}'
+                    GROUP BY `CLASSIFICAÇÃO SINAPI`
+                    HAVING TOTAL > 0
+                    ORDER BY TOTAL DESC
+                    LIMIT 10;
+                    """, conn=CONN)
+        
+        # # Formata DataFrame
+        # tabela = exibir_tabela(df, tipo='valor')
+            
+        # # DataFrame completo
+        # st.caption('''
+                                    
+        #             ''', text_alignment='left')
+
+        # st.table(tabela, border='horizontal')
+
+        exibir_grafico_barras(df, tipo='valor')
+    else: 
+        st.markdown('## Top 10 por Quantidade')
+        st.markdown('#### O que dá mais trabalho operacional')
+
+        # Quantidade
+        df = get_data(query=f"""
+                    SELECT `CLASSIFICAÇÃO SINAPI`, SUM(`QUANT.`) AS TOTAL
+                    FROM sao_carlos
+                    WHERE `CLASSIFICAÇÃO SINAPI` NOT IN (
+                      'LIVRO SINAPI: CÁLCULOS E PARÂMETROS', 
+                      'NÃO SINAPI', 
+                      'NÃO ENCONTRADO', 
+                      'MATERIAL'
+                      ) 
+                      AND strftime('%Y', DATA) = '{option_safe}'
+                    GROUP BY `CLASSIFICAÇÃO SINAPI`
+                    HAVING TOTAL > 0
+                    ORDER BY TOTAL DESC
+                    LIMIT 10;
+                    """, conn=CONN)
+        
+        # # Formata DataFrame
+        # tabela = exibir_tabela(df, tipo='quantidade')
+            
+        # # DataFrame completo
+        # st.caption('''
+                                    
+        #             ''', text_alignment='left')
+
+        # st.table(tabela, border='horizontal')
+
+        exibir_grafico_barras(df, tipo='quantidade')
+
+# Fecha conexão e encerra sessão
+CONN.close()
